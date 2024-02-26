8d543c464cd316ede4e2a52ae9b2d8555ac0de83
Felipe Morais <121844305+FehMs@users.noreply.github.com>
2024-02-26 19:27:13 -0300

    Update Provider.jsx

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -7,7 +7,7 @@ function Provider({ children }) {
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [isCartVisible, setIsCartVisible] = useState(true);
+  const [isCartVisible, setIsCartVisible] = useState(false);
 
   const value = {
     products, 
